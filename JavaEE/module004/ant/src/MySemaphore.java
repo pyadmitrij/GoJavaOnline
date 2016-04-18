92e774d6cf51ca0964408a78593614ed8245f1d3
pyadmitrij <pyadmitrij@gmail.com>
2016-04-18 15:26:18 +0300

    module004

---
+++ JavaEE/module004/ant/src/MySemaphore.java
@@ -0,0 +1,63 @@
+public class MySemaphore implements Semaphore {
+    private volatile int count = 0;
+    final Object lock = new Object();
+
+    public MySemaphore() {}
+
+    public MySemaphore(int count) {
+        this.count = count;
+    }
+
+    private void workOnTheThread() throws InterruptedException {
+        while (true) {
+            String threadName = Thread.currentThread().getName();
+            if (count > 0) {
+                System.out.println( threadName + " is working now. The number free resolution = " + count--);
+                Thread.sleep(1000);
+                break;
+            }
+            if (count == 0) {
+                System.out.println(threadName + " is waiting");
+                synchronized (lock) {
+                    lock.wait();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void acquire() throws InterruptedException {
+        workOnTheThread();
+    }
+
+    @Override
+    public void acquire(int permits) throws InterruptedException {
+        if (count == 0) {
+            this.count = permits;
+        }
+        workOnTheThread();
+    }
+
+    @Override
+    public void release() {
+        this.count++;
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        System.out.println("Number count: " + count);
+    }
+
+    @Override
+    public void release(int permits) {
+        this.count += permits;
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        System.out.println("Number count: " + count);
+    }
+
+    @Override
+    public int getAvailablePermits() {
+        return this.count;
+    }
+}
